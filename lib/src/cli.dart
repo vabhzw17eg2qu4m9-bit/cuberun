266fa5eaa7de0838ea6a835a3c55195a1ad5b71d
vabhzw17eg2qu4m9-bit <vabhzw17eg2qu4m9@gmail.com>
2026-09-22 14:28:09 +0300

    chore: bump version to 0.3.0

--- lib/src/cli.dart
+++ lib/src/cli.dart
@@ -20,7 +20,7 @@ import 'sbpl.dart';
 import 'stage.dart';
 
 /// cube-sandbox version (kept in one place for `--version` and CI smoke).
-const String kCubeSandboxVersion = '0.2.1';
+const String kCubeSandboxVersion = '0.3.0';
 
 const String _usage =
     '''
